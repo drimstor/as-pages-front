14edc22249c4d87ac52d0805d33b65302f3ddab9
Vyacheslav <70888287+drimstor@users.noreply.github.com>
2022-07-15 13:31:09 +0300

    Add files via upload

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -2,9 +2,7 @@ import React, { useContext, useState } from 'react';
 import { GetStart, PricingCard, SeeDown } from '../components';
 import { ModalContext } from '../contexts';
 
-import { styled } from '@mui/material/styles';
-
-import { Button, FormControlLabel, Switch } from '@mui/material';
+import { Button, Switch } from '@mui/material';
 
 function Pricing() {
   const { openModal } = useContext(ModalContext);
@@ -27,59 +25,10 @@ function Pricing() {
     }
   });
 
-  const IOSSwitch = styled((props) => (
-    <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
-  ))(({ theme }) => ({
-    width: 42,
-    height: 26,
-    padding: 0,
-    '& .MuiSwitch-switchBase': {
-      padding: 0,
-      margin: 2,
-      transitionDuration: '300ms',
-      '&.Mui-checked': {
-        transform: 'translateX(16px)',
-        color: '#fff',
-        '& + .MuiSwitch-track': {
-          backgroundColor: theme.palette.mode === 'dark' ? '#2196f3' : '#2196f3',
-          opacity: 1,
-          border: 0,
-        },
-        '&.Mui-disabled + .MuiSwitch-track': {
-          opacity: 0.5,
-        },
-      },
-      '&.Mui-focusVisible .MuiSwitch-thumb': {
-        color: '#33cf4d',
-        border: '6px solid #fff',
-      },
-      '&.Mui-disabled .MuiSwitch-thumb': {
-        color: theme.palette.mode === 'light' ? theme.palette.grey[100] : theme.palette.grey[600],
-      },
-      '&.Mui-disabled + .MuiSwitch-track': {
-        opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
-      },
-    },
-    '& .MuiSwitch-thumb': {
-      boxSizing: 'border-box',
-      width: 22,
-      height: 22,
-    },
-    '& .MuiSwitch-track': {
-      borderRadius: 26 / 2,
-      backgroundColor: theme.palette.mode === 'light' ? '#E9E9EA' : '#39393D',
-      opacity: 1,
-      transition: theme.transitions.create(['background-color'], {
-        duration: 500,
-      }),
-    },
-  }));
+  const [switchState, setSwitchState] = useState(true);
 
-
-  const [switchState,  setSwitchState] = useState(true);
-
-  function handleSwitch () {
-    setSwitchState(!switchState);
+  function handleSwitch(event) {
+    setSwitchState(event.target.checked);
   }
 
   return (
@@ -96,14 +45,8 @@ function Pricing() {
 
           <div className="pricing__tumbler">
             <span>Montly</span>
-    
-            <FormControlLabel
-              control={<IOSSwitch sx={{ m: 1 }} defaultChecked />}
-              label="Annually (-20%)*"
-              labelPlacement={'End'}
-              checked={switchState}
-              onClick={handleSwitch}
-            />
+            <Switch size='medium' checked={switchState} onChange={handleSwitch} />
+            <span>Annually (-20%)*</span>
           </div>
 
           <div className="pricing__cards">
